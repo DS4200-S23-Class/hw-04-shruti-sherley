f818d350976f227e13aabd8c5ee1752c872b6d2c
shrutikedhar <116131153+shrutikedhar@users.noreply.github.com>
2023-02-15 13:30:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+// changes mousover fill to yellow
 const circles = document.querySelectorAll("circle");
 circles.forEach(function(circle){
   circle.addEventListener("mouseover", function(event) {
     event.target.setAttribute("fill", "yellow");
   });
 
+  // changes fill to purple if not hovering 
   circle.addEventListener("mouseout", function(event) {
     event.target.setAttribute("fill", "purple");
   });
 
-
+// add borders to circles if clicked
   circle.addEventListener("click", function(event) {
     event.target.setAttribute("fill", "green");
     event.target.setAttribute("fill", "green");
@@ -25,6 +27,8 @@ circles.forEach(function(circle){
     document.getElementById("coordinate-display").innerHTML= `Last point clicked<br> (${(xCoord - 20) / 20}, ${(200 - yCoord) / 20})`;
   })
 })
+
+// add x, y coordinate added by user
 document.getElementById("submit-button").addEventListener("click", function(event) {
     event.preventDefault();
     const x = document.getElementById("x-coordinate").value;
@@ -32,22 +36,26 @@ document.getElementById("submit-button").addEventListener("click", function(even
     const xCoord = x * 20 + 20;
     const yCoord = 200 - y * 20;
 
+    // set position and attributes for new points
     const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     circle.setAttribute("cx", xCoord);
     circle.setAttribute("cy", yCoord);
     circle.setAttribute("r", 10);
     circle.setAttribute("fill", "purple");
 
+    // add svg created from user input
     document.querySelector("svg").appendChild(circle);
 
     circle.addEventListener("mouseover", function(event) {
       event.target.setAttribute("fill", "yellow");
     });
 
+    // add mouseout property to new point
     circle.addEventListener("mouseout", function(event) {
       event.target.setAttribute("fill", "purple");
     });
 
+    // add click properties to new point (same as above)
     circle.addEventListener("click", function(event) {
       event.target.setAttribute("fill", "green");
       event.target.setAttribute("fill", "green");
@@ -60,6 +68,8 @@ document.getElementById("submit-button").addEventListener("click", function(even
       } else {
         circle.removeAttribute("stroke");
       }
+
+      // update point to last point clicked
       document.getElementById("coordinate-display").innerHTML= `Last point clicked<br> (${(xCoord - 20) / 20}, ${(200 - yCoord) / 20})`;
      
     });
